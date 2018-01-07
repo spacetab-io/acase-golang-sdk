4addfb452cfcabc5cb591138d3f004e2b476e087
Alexander Tkachev <atkachev@gmail.com>
2018-01-07 23:18:53 +0300

    #NEO-216 Signed-off-by: Alexander Tkachev <atkachev@gmail.com>

--- acaseSts/orderrequest.go
+++ acaseSts/orderrequest.go
@@ -16,6 +16,20 @@ type OrderRequestType struct {
 	AccommodationList	AccommodationListType	`xml:"AccommodationList"`
 }
 
+type OrderAwocRequestType struct {
+	XMLName			xml.Name			`xml:"OrderListRequest"`
+	BuyerId			string				`xml:"BuyerId,attr"`
+	UserId			string				`xml:"UserId,attr"`
+	Password		string				`xml:"Password,attr"`
+	Language		LanguageTypeEnum	`xml:"Language,attr,omitempty"`
+	Id				int					`xml:"Id,attr"`
+	ReferenceNumber	string				`xml:"String20Type,attr"`
+	Currency		CurrencyType		`xml:"Currency"`
+	WhereToPay		SimpleCodeNameType	`xml:"WhereToPay"`
+	Customer		CustomerType		`xml:"Customer"`
+	AwocList		AwocListType		`xml:"AwocList"`
+}
+
 type OrderResponseType struct {
 	XMLName	xml.Name	`xml:"OrderResponse"`
 	Success	SuccessType	`xml:"Success"`
